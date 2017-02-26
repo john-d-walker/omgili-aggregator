8ae0301736f130d7612976537bb6012444aa40cc
John Walker <johnathonwalker@icloud.com>
2017-02-25 20:56:05 -0800

    Wrote code for CSVTool class. Added more test cases

--- lib/csv_tool.rb
+++ lib/csv_tool.rb
@@ -5,12 +5,28 @@ require 'csv'
 class CSVTool
   # Returns csv file contents in a string array, or nil if file does not exist.
   def read(path)
-
+    if File.exists?(path)
+      file_contents = Array.new
+      CSV.read(path).each do |contents|
+        contents.each do |item|
+          file_contents.push(item)
+        end
+      end
+      return file_contents
+    else
+      return nil
+    end
   end
 
   # Writes the array to a .csv file.
   def write(path, array)
-
+    if (path.nil? || array.nil?)
+      return nil
+    end
+    CSV.open(path, 'a') do |file|
+      file << array
+    end
+    return true
   end
 end
 
--- test/test_csv_tool.rb
+++ test/test_csv_tool.rb
@@ -4,38 +4,49 @@ require 'minitest/autorun'
 require_relative '../lib/csv_tool.rb'
 
 class TestCSVTool < Minitest::Test
-  read_contents_test_array = ["1487807963738.zip",
-                              "1487808083762.zip",
-                              "1487808230988.zip",
-                              "1487808401176.zip",
-                              "1487808621686.zip",
-                              "1487808834319.zip",
-                              "1487808843365.zip",
-                              "1487808855456.zip",
-                              "1487809006694.zip",
-                              "1487809228664.zip",
-                              "1487809380526.zip"]
+  attr_reader :csv_tool, :read_contents_test_array
 
   def setup
     @csv_tool = CSVTool.new
+    @read_contents_test_array = ["1487807963738.zip",
+                                 "1487808083762.zip",
+                                 "1487808230988.zip",
+                                 "1487808401176.zip",
+                                 "1487808621686.zip",
+                                 "1487808834319.zip",
+                                 "1487808843365.zip",
+                                 "1487808855456.zip",
+                                 "1487809006694.zip",
+                                 "1487809228664.zip",
+                                 "1487809380526.zip"]
+
   end
 
   def test_that_read_contents_are_correct
-    @csv_tool.read("files/test.csv").each do |item|
-      assert_equal true, read_contents_test_array.include?(item)
+    @csv_tool.read("test/files/test.csv").each do |item|
+      assert_equal true, @read_contents_test_array.include?(item)
     end
   end
 
-  def test_returns_nil_when_path_invalid
-    assert_equal nil, @csv_tool.read("files/test")
+  def test_read_returns_nil_when_path_invalid
+    assert_equal nil, @csv_tool.read("test/files/test.exe")
+  end
+
+  def test_write_returns_nil_when_path_is_nil
+    assert_equal nil, @csv_tool.write(nil, @read_contents_test_array)
+  end
+
+  def test_write_returns_nil_when_array_is_nil
+    assert_equal nil, @csv_tool.write("test/files/write_test.csv", nil)
   end
 
   def test_write_file_contents_are_correct
-    write_path = "files/write_test.csv"
-    @csv_tool.write(write_path, read_contents_test_array)
+    write_path = "test/files/write_test.csv"
+    @csv_tool.write(write_path, @read_contents_test_array)
     @csv_tool.read(write_path).each do |item|
-      assert_equal true, read_contents_test_array.include?(item)
+      assert_equal true, @read_contents_test_array.include?(item)
     end
+    File.delete(write_path)
   end
 end
 
