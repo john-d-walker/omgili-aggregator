9504078936e3229e2024ce7baa83cbf816d4f79e
John Walker <johnathonwalker@icloud.com>
2017-02-26 20:08:54 -0800

    Added RedisPusher tests.

--- lib/redis_pusher.rb
+++ lib/redis_pusher.rb
@@ -1,2 +1,19 @@
 # redis_pusher.rb
 
+require 'redis'
+
+class RedisPusher
+  attr_reader :redis
+  def initialize
+    @redis = Redis.new
+  end
+
+  def single_push(element, list_name)
+
+  end
+
+  def pipeline_push(elements, list_name)
+
+  end
+end
+
--- test/test_redis_pusher.rb
+++ test/test_redis_pusher.rb
@@ -1,2 +1,28 @@
 # test_redis_pusher.rb
 
+require 'minitest/autorun'
+require 'redis'
+require_relative '../lib/redis_pusher.rb'
+
+class TestRedisPusher < Minitest::Test
+  attr_reader :redis_pusher
+
+  def setup
+    @redis_pusher = RedisPusher.new
+  end
+
+  def test_single_push
+    test_string = "Hello, Redis!"
+    redis_list_name = "SINGLE_TEST"
+    @redis_pusher.single_push(test_string, redis_list_name)
+    assert_equal test_string, Redis.new.lrange(redis_list_name, 0, -1)
+  end
+
+  def test_pipelined_push
+    test_array = ["test1", "test2", "test3"]
+    redis_list_name = "PIPE_TEST"
+    @redis_pusher.pipeline_push(redis_list_name, test_array)
+    assert_equal test_array, Redis.new.lrange(redis_list_name, 0, -1)
+  end
+end
+
