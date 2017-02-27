148c2e72ebe7412ec793842baea35827148cd1e8
John Walker <johnathonwalker@icloud.com>
2017-02-26 22:37:22 -0800

    Added OmgiliAggregator tests and generate_download_list method.

--- lib/omgili_aggregator.rb
+++ lib/omgili_aggregator.rb
@@ -1,7 +1,26 @@
 # omgili_aggregator.rb
 
 require_relative 'scraper'
+require_relative 'csv_tool'
 require_relative 'download_manager'
 require_relative 'unzipper'
 require_relative 'redis_pusher'
+
+class OmgiliAggregator
+  def generate_download_list(csv_data, scrape_data)
+    results = Array.new
+
+    scrape_data.each do |item|
+      unless csv_data.include?(item)
+        results.push(item)
+      end
+    end
+
+    return results
+  end
+
+  def run
+
+  end
+end
 
